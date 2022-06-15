40aa2f1a669afebd9c51f65a4505b719dd4d5f38
Hakan Alif Pramudya <hakanalifpramudya13@gmail.com>
2022-06-15 19:27:28 +0700

    Graf

--- Pert-12/Graph/src/main/java/GraphGeneric/Graph.java
+++ Pert-12/Graph/src/main/java/GraphGeneric/Graph.java
@@ -9,34 +9,25 @@ package GraphGeneric;
  *
  * @author USER
  */
-// Java program to implement Graph
-// with the help of Generics
-
 import java.util.*;
 
-class Graph<T> {
+class Graph<T>{
 
-	// We use Hashmap to store the edges in the graph
 	private Map<T, List<T> > map = new HashMap<>();
 
-	// This function adds a new vertex to the graph
-	public void addVertex(T s)
-	{
+	public void addVertex(T s){
+	
 		map.put(s, new LinkedList<T>());
 	}
 
-	// This function adds the edge
-	// between source to destination
-	public void addEdge(T source,
-						T destination,
-						boolean bidirectional)
-	{
-
-		if (!map.containsKey(source))
+	public void addEdge(T source, T destination, boolean bidirectional){					
+		if (!map.containsKey(source)){
 			addVertex(source);
+                }
 
-		if (!map.containsKey(destination))
+		if (!map.containsKey(destination)){
 			addVertex(destination);
+                }
 
 		map.get(source).add(destination);
 		if (bidirectional == true) {
@@ -44,17 +35,11 @@ class Graph<T> {
 		}
 	}
 
-	// This function gives the count of vertices
-	public void getVertexCount()
-	{
-		System.out.println("Graph punya: "
-						+ map.keySet().size()
-						+ " vertex");
+	public void getVertexCount(){
+		System.out.println("Graph punya: " + map.keySet().size()+ " vertex");
 	}
 
-	// This function gives the count of edges
-	public void getEdgesCount(boolean bidirection)
-	{
+	public void getEdgesCount(boolean bidirection){
 		int count = 0;
 		for (T v : map.keySet()) {
 			count += map.get(v).size();
@@ -62,42 +47,29 @@ class Graph<T> {
 		if (bidirection == true) {
 			count = count / 2;
 		}
-		System.out.println("Graph punya: "
-						+ count
-						+ " edges.");
+		System.out.println("Graph punya: "+ count + " edges.");
 	}
 
-	// This function gives whether
-	// a vertex is present or not.
-	public void hasVertex(T s)
-	{
+	public void hasVertex(T s){
 		if (map.containsKey(s)) {
-			System.out.println("Graph punya: "
-							+ s + " untuk vertex.");
+			System.out.println("Graph punya: "+ s + " untuk vertex.");
 		}
 		else {
-			System.out.println("Graph tidak punya: "
-							+ s + " untuk vertex.");
+			System.out.println("Graph tidak punya: " + s + " untuk vertex.");
 		}
 	}
 
-	// This function gives whether an edge is present or not.
-	public void hasEdge(T s, T d)
-	{
+	public void hasEdge(T s, T d){
 		if (map.get(s).contains(d)) {
-			System.out.println("Graph punya edges diantara: "
-							+ s + " dan " + d + ".");
+			System.out.println("Graph punya edges diantara: " + s + " dan " + d + ".");
 		}
 		else {
-			System.out.println("Graph punya edges diantara: "
-							+ s + " dan " + d + ".");
+			System.out.println("Graph punya edges diantara: " + s + " dan " + d + ".");
 		}
 	}
 
-	// Prints the adjancency list of each vertex.
 	@Override
-	public String toString()
-	{
+	public String toString(){
 		StringBuilder builder = new StringBuilder();
 
 		for (T v : map.keySet()) {
